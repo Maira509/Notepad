f2ae941c424dcfd3e48dd4f778c856b206ed03de
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-25 17:25:53 +0500

    Share on whatsapp implemented

--- app/src/main/java/com/example/notes/page.java
+++ app/src/main/java/com/example/notes/page.java
@@ -2,13 +2,47 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Editable;
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class page extends AppCompatActivity {
-
+    private Button btn1, btn2;
+    private EditText editText;
+    public String value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
-    }
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.textView);
+        value = editText.getText().toString();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.setPackage("com.whatsapp");
+                intent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+                startActivity(intent);
+            }
+    });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(page.this, Activity2.class);
+                LayoutParams lparams = new LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                TextView tv=new TextView(this);
+                tv.setLayoutParams(lparams);
+                tv.setText("test");
+                this.m_vwJokeLayout.addView(tv);
+                startActivity(intent);
+            }
+        });
+}
 }
